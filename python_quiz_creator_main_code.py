a10c0b3037255acb48c5fc8e8ea5e9fc087b37fe
cnriveraa <rivera.cn18@gmail.com>
2025-04-09 11:51:36 +0800

    using for loop, prompt user to enter the answers

--- python_quiz_creator_main_code.py
+++ python_quiz_creator_main_code.py
@@ -7,8 +7,10 @@ while True: # start an infinite loop to keep asking for questions
     question = input("Enter the question: ")     # prompt for the user to enter the questions
     
     asnwers = {} # a dictionary to store the answers
-
-# prompt for the user to enter the answers
+    
+    for option in ['a', 'b', 'c', 'd']:
+        answer = input(f"Enter answer for option {option}: ")    # prompt for the user to enter the answers
+        
 # store the answers in the dictionary
 # prompt for the correct answer
 # validate the correct answer
