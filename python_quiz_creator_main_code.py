3d90afeefd663e4bcc6f1ab2e4538e42929a4039
cnriveraa <rivera.cn18@gmail.com>
2025-04-09 11:41:31 +0800

    pseudocode added

---
+++ python_quiz_creator_main_code.py
@@ -0,0 +1,12 @@
+# python_quiz_creator_main_code.py
+
+# output file name
+# prompt for the user to enter the questions
+# a dictionary to store the answers
+# prompt for the user to enter the answers
+# store the answers in the dictionary
+# prompt for the correct answer
+# validate the correct answer
+# error message if the answer is not valid
+# add the questions and answers to the text file
+# ask if the user wants another question
