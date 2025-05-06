d2f42e1a00f3897aa07c632ac4152f35a70bcd34
cnriveraa <rivera.cn18@gmail.com>
2025-05-06 15:13:10 +0800

    pseudocode added

---
+++ two_python_quiz_creator.py
@@ -0,0 +1,5 @@
+# import necessary libraries
+# add function to load question from JSON file
+# add function to ask a question and check the answer
+# main function to load the questions and pick a random one
+# entry point check for running the program
