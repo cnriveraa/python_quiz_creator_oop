fb69bd1670aae854356e259cbdb1632ca7d4ae0e
cnriveraa <rivera.cn18@gmail.com>
2025-05-06 15:53:36 +0800

    import necessary libraries

--- two_python_quiz_creator.py
+++ two_python_quiz_creator.py
@@ -1,4 +1,8 @@
 # import necessary libraries
+import json
+import random
+import os
+
 # add function to load question from JSON file
 # add function to ask a question and check the answer
 # main function to load the questions and pick a random one
